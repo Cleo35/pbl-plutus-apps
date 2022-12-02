fc2b5f19496709bb4ae8c2c10d3bf6cc8173e680
Markus Läll <markus.lall@iohk.io>
2022-12-02 15:47:26 +0200

    Add epoch stakepool size indexer
    The `toEvents` and `getStakeMap` make up the core of the
    implementation in Marconi.Index.EpochStakepoolSize.

--- marconi/src/Marconi/Indexers.hs
+++ marconi/src/Marconi/Indexers.hs
@@ -19,10 +19,15 @@ import Control.Concurrent.STM (atomically)
 import Control.Concurrent.STM.TChan (TChan, dupTChan, newBroadcastTChanIO, readTChan, writeTChan)
 import Control.Lens.Operators ((^.))
 import Control.Monad (void)
+import Control.Monad.Trans.Class (lift)
+import Data.Foldable (foldl')
 import Data.List (findIndex)
 import Data.Map (Map)
 import Data.Map qualified as Map
-import Data.Maybe (fromMaybe, mapMaybe)
+import Data.Maybe (catMaybes, fromMaybe, mapMaybe)
+import Data.Set (Set)
+import Data.Set qualified as Set
+import Database.SQLite.Simple qualified as SQL
 import Streaming.Prelude qualified as S
 
 import Cardano.Api (Block (Block), BlockHeader (BlockHeader), BlockInMode (BlockInMode), CardanoMode, Hash, ScriptData,
@@ -31,9 +36,11 @@ import Cardano.Api qualified as C
 import "cardano-api" Cardano.Api.Shelley qualified as Shelley
 import Cardano.Ledger.Alonzo.TxWitness qualified as Alonzo
 import Cardano.Streaming (ChainSyncEvent (RollBackward, RollForward))
-import Control.Concurrent.STM.TMVar (TMVar)
+import Cardano.Streaming qualified as CS
+import Control.Concurrent.STM.TMVar (TMVar, putTMVar)
 import Marconi.Index.Datum (DatumIndex)
 import Marconi.Index.Datum qualified as Datum
+import Marconi.Index.EpochStakepoolSize qualified as EpochStakepoolSize
 import Marconi.Index.ScriptTx qualified as ScriptTx
 import Marconi.Index.Utxo qualified as Utxo
 import Marconi.Types (TargetAddresses)
@@ -177,24 +184,49 @@ newtype UtxoQueryTMVar = UtxoQueryTMVar
     { unUtxoIndex  :: TMVar Utxo.UtxoIndex      -- ^ for query thread to access in-memory utxos
     }
 
+epochStakepoolSizeWorker :: FilePath -> Worker
+epochStakepoolSizeWorker configPath Coordinator{_barrier} tchan dbPath = do
+  dbCon <- SQL.open dbPath
+  (env, initialLedgerStateHistory) <- CS.getEnvAndInitialLedgerStateHistory configPath
+  let
+    indexer = chainSyncEvents
+      & CS.foldLedgerState env initialLedgerStateHistory C.QuickValidation
+      & EpochStakepoolSize.toEvents
+      & EpochStakepoolSize.sqlite dbCon
+
+  S.effects indexer
+  where
+    -- Read blocks from TChan, emit them as a stream.
+    chainSyncEvents :: S.Stream (S.Of (ChainSyncEvent (BlockInMode CardanoMode))) IO ()
+    chainSyncEvents = do
+      lift $ signalQSemN _barrier 1
+      S.yield =<< (lift $ atomically $ readTChan tchan)
+      chainSyncEvents
+
 combinedIndexer
   :: Maybe FilePath
   -> Maybe FilePath
   -> Maybe FilePath
+  -> Maybe FilePath
   -> Maybe TargetAddresses
+  -> Maybe FilePath -- ^ Node config path, required for epoch stakepool size indexer
   -> S.Stream (S.Of (ChainSyncEvent (BlockInMode CardanoMode))) IO r
   -> IO ()
-combinedIndexer utxoPath datumPath scriptTxPath maybeTargetAddresses = combineIndexers remainingIndexers
+combinedIndexer utxoPath datumPath scriptTxPath epochStakepoolSizePath maybeTargetAddresses maybeConfigPath = combineIndexers remainingIndexers
   where
     liftMaybe (worker, maybePath) = case maybePath of
       Just path -> Just (worker, path)
       _         -> Nothing
+    epochStakepoolSizeIndexer = case maybeConfigPath of
+      Just configPath -> [(epochStakepoolSizeWorker configPath, epochStakepoolSizePath)]
+      _               -> []
+
     pairs =
         [
             (utxoWorker pure maybeTargetAddresses, utxoPath)
             , (datumWorker, datumPath)
             , (scriptTxWorker (\_ -> pure []), scriptTxPath)
-        ]
+        ] <> epochStakepoolSizeIndexer
     remainingIndexers = mapMaybe liftMaybe pairs
 
 combineIndexers
