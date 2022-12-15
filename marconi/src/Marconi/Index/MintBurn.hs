36741fdd7c767ed0df6808d9fe62517c826d1510
Markus Läll <markus.lall@iohk.io>
2022-12-15 19:10:20 +0200

    wip Thursday

--- marconi/src/Marconi/Index/MintBurn.hs
+++ marconi/src/Marconi/Index/MintBurn.hs
@@ -5,7 +5,9 @@ module Marconi.Index.MintBurn where
 import Cardano.Api qualified as C
 import Cardano.Api.Shelley qualified as C
 import Cardano.Ledger.Alonzo.Data qualified as L
+import Cardano.Ledger.Alonzo.Scripts qualified as LA
 import Cardano.Ledger.Alonzo.TxWitness qualified as L
+import Cardano.Ledger.Core qualified as L
 import Control.Monad.IO.Class (liftIO)
 import Data.Map qualified as Map
 import Data.Word (Word64)
@@ -29,41 +31,54 @@ toUpdate :: C.BlockInMode C.CardanoMode -> [TxMintEvent]
 toUpdate (C.BlockInMode (C.Block (C.BlockHeader slotNo _ blockNo) txs :: C.Block era) _ :: C.BlockInMode C.CardanoMode) = do
   tx@(C.Tx txb@(C.TxBody txbc) _) <- txs
 
+  let mkTxMintEvent policyId assetName quantity redeemerIx redeemerData = TxMintEvent
+        { txMintEventSlot = slotNo
+        , txMintEventBlockNo = blockNo
+        , txMintEventTxId = C.getTxId txb
+        , txMintEventPolicyId = policyId
+        , txMintEventAssetName = assetName
+        , txMintEventQuantity = quantity
+        , txMintEventRedeemerIdx = redeemerIx
+        , txMintEventRedeemerData = redeemerData
+      }
+
   -- AssetId and Quantity from txMintValue
   (assetId, quantity) :: (C.AssetId, C.Quantity) <- case C.txMintValue txbc of
     C.TxMintValue _ (v :: C.Value) _ -> C.valueToList v
   case assetId of
-    C.AdaAssetId -> []
-    C.AssetId policyId assetName -> pure $ TxMintEvent
-      { txMintEventSlot = slotNo
-      , txMintEventBlockNo = blockNo
-      , txMintEventTxId = C.getTxId txb
-      , txMintEventPolicyId = policyId
-      , txMintEventAssetName = assetName
-      , txMintEventQuantity = quantity
-      -- , txMintEventRedeemerIdx :: Word64
-      -- , txMintEventRedeemerData :: C.ScriptData
-      }
+    C.AdaAssetId                 -> []
+    C.AssetId policyId assetName -> pure $ mkTxMintEvent policyId assetName quantity undefined undefined
 
-  -- From TxOuts
+  -- From TxBodyContent{txOuts}: But this is WRONG, as this only transfers already existing asset
   txOut <- C.txOuts txbc
   (policyId, assetName, quantity) <- txOutValue txOut
-  pure $ TxMintEvent
-    { txMintEventSlot = slotNo
-    , txMintEventBlockNo = blockNo
-    , txMintEventTxId = C.getTxId txb
-    , txMintEventPolicyId = policyId
-    , txMintEventAssetName = assetName
-    , txMintEventQuantity = quantity
-    -- , txMintEventRedeemerIdx :: Word64
-    -- , txMintEventRedeemerData :: C.ScriptData
-    }
+  pure $ mkTxMintEvent policyId assetName quantity undefined undefined
 
+  -- From real TxBody
+  let
+    scripts@(s1 : _) = txScripts txb :: [L.Script (C.ShelleyLedgerEra era)]
+
+    rm = txRedeemers txb
+    rl = Map.assocs rm -- ptr word is index of script?
+    _ = map (\((L.RdmrPtr tag w), _) -> undefined) rl
+    _ = txDatums txb
+
+  pure $ mkTxMintEvent policyId assetName quantity undefined undefined
   where
 
-    txRedeemer :: C.TxBody era -> [L.Data (C.ShelleyLedgerEra era)]
-    txRedeemer (C.ShelleyTxBody _ _ _ txScriptData _ _) = case txScriptData of
-      C.TxBodyScriptData _proof datum redeemers -> map fst $ Map.elems $ L.unRedeemers redeemers
+    -- * TxBody part getters
+
+--    txRedeemers :: C.TxBody era -> [L.Data (C.ShelleyLedgerEra era)]
+    txRedeemers (C.ShelleyTxBody _ _ _ txScriptData _ _) = case txScriptData of
+      C.TxBodyScriptData _proof datum redeemers -> L.unRedeemers redeemers
+
+    -- UNUSED
+    txDatums :: C.TxBody era -> [L.Data (C.ShelleyLedgerEra era)]
+    txDatums (C.ShelleyTxBody _ _ _ txScriptData _ _) = case txScriptData of
+      C.TxBodyScriptData _proof datums redeemers -> Map.elems $ L.unTxDats datums
+
+    txScripts :: C.TxBody era -> [L.Script (C.ShelleyLedgerEra era)]
+    txScripts (C.ShelleyTxBody _ _ scripts _ _ _) = scripts
 
     txOutValue :: C.TxOut ctx era -> [(C.PolicyId, C.AssetName, C.Quantity)]
     txOutValue txOut =  case txOut of
@@ -82,6 +97,15 @@ toUpdate (C.BlockInMode (C.Block (C.BlockHeader slotNo _ blockNo) txs :: C.Block
       case assetId of
         C.AdaAssetId                 -> []
         C.AssetId policyId assetName -> pure (policyId, assetName, quantity)
+
+    -- * Real TxBody
+
+    fromTxBody txBody = let
+      scripts@(s1 : _) = txScripts txBody
+      rm = txRedeemers txBody
+      rl = Map.assocs rm -- ptr word is index of script?
+      _ = txDatums txBody
+      in TxMintEvent
 
 
 
