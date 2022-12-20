f0a74044009865a8b32ffb22adbef25fb1c92d2c
Markus Läll <markus.lall@iohk.io>
2022-12-20 19:37:12 +0200

    wip Tuesday

--- marconi/src/Marconi/Index/MintBurn.hs
+++ marconi/src/Marconi/Index/MintBurn.hs
@@ -7,6 +7,7 @@
 module Marconi.Index.MintBurn where
 
 import Control.Monad.IO.Class (liftIO)
+import Data.Function ((&))
 import Data.Map qualified as Map
 import Data.Word (Word64)
 import Database.SQLite.Simple qualified as SQL
@@ -23,6 +24,9 @@ import Cardano.Ledger.Alonzo.Scripts qualified as LA
 import Cardano.Ledger.Alonzo.Tx qualified as LA
 import Cardano.Ledger.Alonzo.TxWitness qualified as LA
 
+import Ouroboros.Consensus.Shelley.Eras qualified as OEra
+
+import Cardano.Ledger.Mary.Value qualified as LM
 
 data TxMintEvent = TxMintEvent
   { txMintEventSlot         :: C.SlotNo
@@ -54,113 +58,98 @@ toUpdate (C.BlockInMode (C.Block (C.BlockHeader slotNo _ blockNo) txs :: C.Block
       }
 
   -- AssetId and Quantity from txMintValue
-  (assetId, quantity) :: (C.AssetId, C.Quantity) <- case C.txMintValue txbc of
-    C.TxMintValue _ (v :: C.Value) _ -> C.valueToList v
-  case assetId of
-    C.AdaAssetId                 -> []
-    C.AssetId policyId assetName -> pure $ mkTxMintEvent policyId assetName quantity undefined undefined
+  -- (assetId, quantity) :: (C.AssetId, C.Quantity) <- case C.txMintValue txbc of
+  --   C.TxMintValue _ (v :: C.Value) _ -> C.valueToList v
+  -- case assetId of
+  --   C.AdaAssetId                 -> []
+  --   C.AssetId policyId assetName -> pure $ mkTxMintEvent policyId assetName quantity undefined undefined
 
   -- From TxBodyContent{txOuts}: But this is WRONG, as this only transfers already existing asset
-  txOut <- C.txOuts txbc
-  (policyId, assetName, quantity) <- txOutValue txOut
-  pure $ mkTxMintEvent policyId assetName quantity undefined undefined
+  -- txOut <- C.txOuts txbc
+  -- (policyId, assetName, quantity) <- txOutValue txOut
+  -- pure $ mkTxMintEvent policyId assetName quantity undefined undefined
 
   -- From real TxBody
-  let
-    scripts@(s1 : _) = txScripts txb :: [LCore.Script (C.ShelleyLedgerEra era)]
+  -- let
+  --   scripts@(s1 : _) = txScripts txb :: [LCore.Script (C.ShelleyLedgerEra era)]
 
-    rm = txRedeemers txb
-    rl = Map.assocs rm -- ptr word is index of script?
-    _ = map (\((LA.RdmrPtr tag w), _) -> undefined) rl
-    _ = txDatums txb
+  --   rm = txRedeemers txb
+  --   rl = Map.assocs rm -- ptr word is index of script?
+  --   _ = map (\((LA.RdmrPtr tag w), _) -> undefined) rl
+  --   _ = txDatums txb
 
-  pure $ mkTxMintEvent policyId assetName quantity undefined undefined
+  -- pure $ mkTxMintEvent policyId assetName quantity undefined undefined
 
   -- From real Tx 2
   let
     _ = txb :: C.TxBody era
-    _ = case txb of
+    result = case txb of
       C.ShelleyTxBody era tx _ _ _ _ -> let
-        _ = era :: C.ShelleyBasedEra era
-        _ = tx :: LCore.TxBody (C.ShelleyLedgerEra era)
---        _ = getLedgerEraConstraint era $ let LA.TxBody{} = tx in undefined
-        _ = case era of
-          C.ShelleyBasedEraShelley -> undefined
-          C.ShelleyBasedEraAllegra -> undefined
-          C.ShelleyBasedEraMary -> undefined
+        -- _ = era :: C.ShelleyBasedEra era
+        -- _ = tx :: LCore.TxBody (C.ShelleyLedgerEra era)
+        in case era of
+          C.ShelleyBasedEraShelley -> []
+          C.ShelleyBasedEraAllegra -> []
+          C.ShelleyBasedEraMary -> []
           C.ShelleyBasedEraAlonzo -> let
+              mintRedeemers :: [(Word64, (LA.Data (C.ShelleyLedgerEra C.AlonzoEra), LA.ExUnits))]
+              mintRedeemers = txRedeemers txb
+                & Map.toList
+                & filter (\(LA.RdmrPtr tag _, _) -> tag == LA.Mint)
+                & map (\(LA.RdmrPtr _ w, a) -> (w, a))
+              LM.Value _ m = LA.mint tx :: LM.Value (LEra.Crypto (LA.AlonzoEra LCrypto.StandardCrypto))
+              _ = m :: Map.Map (LM.PolicyID LCrypto.StandardCrypto) (Map.Map LM.AssetName Integer)
+              policyIdList = Map.toList m
+              policyRedeemers :: [((LM.PolicyID LCrypto.StandardCrypto, Map.Map LM.AssetName Integer), (LA.Data OEra.StandardAlonzo, LA.ExUnits))]
+              policyRedeemers = map (\(index, data_) -> (policyIdList !! fromIntegral index, data_)) mintRedeemers
 
-            tx'@(LA.TxBody{}) = tx
-            _ = tx' :: (eraL ~ LA.AlonzoEra LCrypto.StandardCrypto, LEra.Era eraL) => LA.TxBody eraL
-            _ = tx' :: _
+            in policyRedeemers
 
---            f :: LEra.Era era => tx0 -> LA.ScriptPurpose (LEra.Crypto era) -> Maybe (LA.Data era, LA.ExUnits)
---            f a b = LA.indexedRdmrs a b
+          C.ShelleyBasedEraBabbage -> []
 
---            _ = f tx' undefined
+      _ -> [] -- ByronTxBody is not exported but as it's the only other data constructor then _ matches it.
 
-            in undefined -- undefined
-          C.ShelleyBasedEraBabbage -> undefined
+  ((LM.PolicyID scriptHash, _), (redeemer, _)) :: ((LM.PolicyID LCrypto.StandardCrypto, Map.Map LM.AssetName Integer), (LA.Data OEra.StandardAlonzo, LA.ExUnits)) <- result
 
-
-
---
-
---         _ = getLedgerEraConstraint era $ let
---           tx1 = tx :: (L.Era (C.ShelleyLedgerEra era), C.ShelleyLedgerEra era ~ erax) => L.TxBody erax
---           LA.TxBody{} = tx1
-
--- --          _ = LA.indexedRdmrs tx1 undefined
---           in undefined
-
-        in undefined -- getLedgerEraConstraint era $ \lera -> undefined
-        -- let
-        --   _ =
-        --   in undefined
+  let _ = mkTxMintEvent policyId
 
   undefined
   where
 
     -- * TxBody part getters
 
---    txRedeemers :: C.TxBody era -> [L.Data (C.ShelleyLedgerEra era)]
+    txRedeemers :: C.TxBody era -> Map.Map LA.RdmrPtr (LA.Data (C.ShelleyLedgerEra era), LA.ExUnits)
     txRedeemers (C.ShelleyTxBody _ _ _ txScriptData _ _) = case txScriptData of
       C.TxBodyScriptData _proof datum redeemers -> LA.unRedeemers redeemers
 
     -- UNUSED
-    txDatums :: C.TxBody era -> [LA.Data (C.ShelleyLedgerEra era)]
-    txDatums (C.ShelleyTxBody _ _ _ txScriptData _ _) = case txScriptData of
-      C.TxBodyScriptData _proof datums redeemers -> Map.elems $ LA.unTxDats datums
+    -- txDatums :: C.TxBody era -> [LA.Data (C.ShelleyLedgerEra era)]
+    -- txDatums (C.ShelleyTxBody _ _ _ txScriptData _ _) = case txScriptData of
+    --   C.TxBodyScriptData _proof datums redeemers -> Map.elems $ LA.unTxDats datums
 
-    txScripts :: C.TxBody era -> [LCore.Script (C.ShelleyLedgerEra era)]
-    txScripts (C.ShelleyTxBody _ _ scripts _ _ _) = scripts
+    -- txScripts :: C.TxBody era -> [LCore.Script (C.ShelleyLedgerEra era)]
+    -- txScripts (C.ShelleyTxBody _ _ scripts _ _ _) = scripts
 
-    txOutValue :: C.TxOut ctx era -> [(C.PolicyId, C.AssetName, C.Quantity)]
-    txOutValue txOut =  case txOut of
-      C.TxOut _proof value' _datum _referenceScript -> case value' of
-        C.TxOutAdaOnly _proof _lovelace -> undefined
-        C.TxOutValue _proof value''     -> fromValue value''
+    -- txOutValue :: C.TxOut ctx era -> [(C.PolicyId, C.AssetName, C.Quantity)]
+    -- txOutValue txOut =  case txOut of
+    --   C.TxOut _proof value' _datum _referenceScript -> case value' of
+    --     C.TxOutAdaOnly _proof _lovelace -> undefined
+    --     C.TxOutValue _proof value''     -> fromValue value''
 
 
 --    f2 :: C.TxBodyContent build era -> [(C.AssetId, C.Quantity)]
-    f2 txbc = case C.txMintValue txbc of
-      C.TxMintValue _ value _ -> C.valueToList value
+    -- f2 txbc = case C.txMintValue txbc of
+    --   C.TxMintValue _ value _ -> C.valueToList value
 
-    fromValue :: C.Value -> [(C.PolicyId, C.AssetName, C.Quantity)]
-    fromValue value = do
-      (assetId, quantity) <- C.valueToList value
-      case assetId of
-        C.AdaAssetId                 -> []
-        C.AssetId policyId assetName -> pure (policyId, assetName, quantity)
+    -- fromValue :: C.Value -> [(C.PolicyId, C.AssetName, C.Quantity)]
+    -- fromValue value = do
+    --   (assetId, quantity) <- C.valueToList value
+    --   case assetId of
+    --     C.AdaAssetId                 -> []
+    --     C.AssetId policyId assetName -> pure (policyId, assetName, quantity)
 
     -- * Real TxBody
 
-    fromTxBody txBody = let
-      scripts@(s1 : _) = txScripts txBody
-      rm = txRedeemers txBody
-      rl = Map.assocs rm -- ptr word is index of script?
-      _ = txDatums txBody
-      in TxMintEvent
 
 
 
